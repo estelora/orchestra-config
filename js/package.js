79cc13452fa0ac76f1e4014b85fee980bdd49b9a
Lydia Joslin <lydia.joslin@mindflash.com>
2017-07-31 16:43:39 -0700

    wip(package): added sudo to all functions.

--- js/package.js
+++ js/package.js
@@ -1,11 +1,9 @@
 var shell = require('shelljs');
 
 function install(package) {
-  shell.exec('apt-get -y install ' + package);
+  shell.exec('sudo apt-get -y install ' + package);
 }
 
 function remove(package) {
-  shell.exec('apt-get -y remove ' + package);
+  shell.exec('sudo apt-get -y remove ' + package);
 }
-
-
