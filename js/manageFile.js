a685f2cbf01e693b5afd979cec0d42b1e8f4d122
Lydia Joslin <lydia.joslin@mindflash.com>
2017-07-31 17:17:25 -0700

    wip(manageFile): added filepath to console.log statements.

--- js/manageFile.js
+++ js/manageFile.js
@@ -10,14 +10,14 @@ function watchFile(path) {
 function writeFile(filepath, contents) {
   fs.writeFile(filepath, contents, (err) => {
     if (err) throw err;
-    console.log('The file has been saved!');
+    console.log('The file ' + filepath + ' has been saved.');
   });
 }
 
 function deleteFile(filepath) {
   fs.unlink(filepath, (err) => {
     if (err) throw err;
-    console.log('The file has been deleted!');
+    console.log('The file ' + filepath + ' has been deleted!');
   });
 }
 
