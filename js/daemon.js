2b8665550f17cea48daf196f1b3814626910a21f
Lydia Joslin <lydia.joslin@mindflash.com>
2017-07-31 17:20:18 -0700

    wip(var): renamed shell variable to sh.

--- js/daemon.js
+++ js/daemon.js
@@ -1,14 +1,14 @@
-var shell = require('shelljs');
+var sh = require('shelljs');
 
 function start(daemon) {
-  shell.exec('sudo service ' + daemon + ' start');
+  sh.exec('sudo service ' + daemon + ' start');
 }
 
 function stop(daemon) {
-  shell.exec('sudo service ' + daemon + ' stop');
+  sh.exec('sudo service ' + daemon + ' stop');
 }
 
 function restart(daemon) {
-  shell.exec('sudo service ' + daemon + ' restart');
+  sh.exec('sudo service ' + daemon + ' restart');
 }
 
--- js/package.js
+++ js/package.js
@@ -1,9 +1,11 @@
-var shell = require('shelljs');
+var sh = require('shelljs');
 
 function install(package) {
-  shell.exec('sudo apt-get -y install ' + package);
+  sh.exec('sudo apt-get -y install ' + package);
 }
 
 function remove(package) {
-  shell.exec('sudo apt-get -y remove ' + package);
+  sh.exec('sudo apt-get -y remove ' + package);
 }
+
+
