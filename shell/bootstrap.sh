b1e957a7c731880f6ddf62d9e90fd84d49215a87
Lydia Joslin <lydia.joslin@mindflash.com>
2017-08-01 17:13:46 -0700

    config(bootstrap): updated bootstrap.sh for node 8.

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
+
+# clean the package manager cache
+ sudo apt-get clean
+
 # check for updates
-sudo apt-get update
+sudo apt-get -y update
 
-# install node
+# grab source for node 8
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+
+# install node & npm
 sudo apt-get -y install nodejs
-
-# create symlink for node
-sudo ln -s /usr/bin/nodejs /usr/bin/node
-
-# install npm
-sudo apt-get -y install npm
