0f07f74279c16e9c85a39dbfc43814b1c9ab80ac
Lydia Joslin <lydia.joslin@mindflash.com>
2017-08-06 19:15:18 -0700

    feat(arrangement): added hello-arrangement-php.js as an example.

---
+++ js/hello-php-arrangement.js
@@ -0,0 +1,21 @@
+var orchestra = require('./orchestra.js');
+
+var apacheRoot = '/var/www/html';
+var apacheConf = '/etc/apache2/apache2.conf';
+var relevantFiles = [`${apacheRoot}/index.php`, `${apacheRoot}/index.html`, apacheConf];
+
+var php = `<?php 
+  header("Content-Type: text/plain"); 
+  echo "Hello, world!\n"; ?>`;
+
+// Install packages
+orchestra.installPackage('apache2');
+orchestra.installPackage('libapache2-mod-php5');
+orchestra.installPackage('php5');
+
+// Configure Apache Files
+orchestra.removeFile(`${apacheRoot}/index.html`, 'apache2');
+orchestra.writeFileContents(`${apacheRoot}/index.php`, php);
+
+// Restart the Apache service, if needed
+orchestra.restartDaemon('apache2', relevantFiles);
