fdb1ff6a55d5d6c5f5c1b629d6ed8f04400def2b
Lydia Joslin <lydia.joslin@mindflash.com>
2017-08-06 18:01:53 -0700

    feat(restart): set orchestra to restart the daemon  if any file in the array files returns true.

--- js/orchestra.js
+++ js/orchestra.js
@@ -2,114 +2,131 @@ var fs = require('fs');
 var sh = require('shelljs');
 sh.config.silent = true;
 
-/**
- * TODO: Make this an npm library
- * TODO: Add template string functionality.
- */
+var orchestraCacheHome = `${process.env.HOME}/.cache/orchestra`;
+var packageCmd = 'sudo apt-get -y';
+var list = 'apt list';
+
 
 // Package Manager
 exports.installPackage = function install(package) {
-  var packageStatus = checkPackageStatus(package);
-  if(packageStatus.includes('rc  ' + package)) {
-    sh.exec('sudo apt-get -y install ' + package);
-    console.log('Successfully installed ' + package + '.');
-  } else if(packageStatus.includes('ii  ' +  package)) {
-    console.log('Package ' + package + ' already installed.');
-  } else {
-     console.log('Package ' + package + ' not found, will not install.');
-  }
-}
+  sh.exec(`${packageCmd} install  + package`);
+};
 
 exports.removePackage = function remove(package) {
-  var packageStatus = checkPackageStatus(package).toString();
-  if(packageStatus.includes('ii  ' + package)) {
-    sh.exec('sudo apt-get -y remove ' + package);
-    console.log('Successfully removed ' + package + '.');
-  } else if (packageStatus.includes('rc  ' + package)) {
-    console.log('Package ' + package + ' not installed, no need to remove.');
-  } else {
-    console.log('Package ' + package + ' not found, will not remove.');
-  }
-}
-
-function checkPackageStatus(package) {
-  var status = sh.exec('dpkg -l ' + package).toString();
-  return status;
-}
+  sh.exec(`${packageCmd} remove  + package`);
+};
 
 // File Manager
-// TODO: Restart the service if the file contents change
 exports.writeFileContents = function write(filepath, contents) {
   /**
   * If file doesn't exist, or if it isn't the same as `contents`
   * overwrite `contents`
   */
-  if(!fs.existsSync(filepath)) {
+  if (!fs.existsSync(filepath)) {
     fs.writeFileSync(filepath, contents);
-    console.log('The file ' + filepath + ' has been saved.');
+    console.log(`The file ${filepath} has been saved.`);
   } else {
-    /**
-    * checkFileMetadata(filepath);
-    * Read the file to determine if its contents match the arrangement contents.
-    */
+    // Read the file to determine if its contents match the arrangement contents.
     var data = fs.readFileSync(filepath, 'utf8');
     /**
     * If the file's contents do not match the arrangement contents,
     * write the arrangement contents to the file.
     */
-    if(data !== contents) {
+    if (data !== contents) {
       fs.writeFileSync(filepath, contents);
-          console.log('The file ' + filepath + ' has been adjusted to match the arrangement.');
+      console.log(
+        `The file ${filepath} has been adjusted to match the arrangement.`
+      );
     }
   }
-}
+};
 
 exports.removeFile = function remove(filepath) {
   if (fs.existsSync(filepath)) {
     fs.unlinkSync(filepath);
-    console.log('The file ' + filepath + ' has been deleted.');
+    console.log(`The file ${filepath} has been deleted.`);
   }
-}
+};
 
 exports.writeFilePermissions = function write(path, mode) {
-  //TODO:  error handling
   fs.chmodSync(path, mode);
-}
+};
 
 exports.writeFileOwner = function write(path, uid, gid) {
-  //TODO: error handling
   fs.chownSync(path, uid, gid);
-}
-
+};
 
 // Daemon Manager
-exports.restartDaemon = function restart(daemon) {
+exports.restartDaemon = function restart(daemon, files = []) {
   /**
-  * Only restart when relevant packages change.
-  * Compare cached and current package metadata to determine if a package changed.
-  */
-  var cache = '/var/cache/orchestra/' + daemon + '.cache';
+   * Only restart when relevant packages change.
+   * Compare cached and current package metadata to determine if a package changed.
+   */
 
-  if (!fs.existsSync(cache)) {
-    sh.exec('sudo service ' + daemon + ' restart');
-    console.log('First restart of ' + daemon + '.');
-    console.log('Creating orchestra cache directory...');
-    sh.exec('sudo mkdir /var/cache/orchestra/');
-    cacheDaemonVersion(daemon);
+  // If any file in the array returns true, restart the daemon.
+  var hasFileChanged = files.some(didFileChange);
+  var cache = `${orchestraCacheHome}/${daemon}.cache`;
+
+  var shouldRestart = false;
+  if (hasFileChanged) {
+    shouldRestart = true;
   }
-  else {
+  if (!fs.existsSync(cache)) {
+    console.log(`First restart of ${daemon}.`);
+    console.log(cache);
+    console.log('Creating orchestra cache directory...');
+    sh.exec(`mkdir -p ${orchestraCacheHome}`);
+    cacheDaemonVersion(daemon);
+    shouldRestart = true;
+  } else {
     var cacheData = fs.readFileSync(cache, 'utf8');
-    var checkPackageData = sh.exec('apt list ' + daemon);
+    var checkPackageData = sh.exec(`${list} daemon`);
     if (cacheData !== checkPackageData.toString()) {
-      sh.exec('sudo service ' + daemon + ' restart');
-      console.log('Package version of ' + daemon + ' changed. Restarting ' + daemon + '.');
+      console.log(`Package version for ${daemon} changed.`);
       cacheDaemonVersion(daemon);
+      shouldRestart = true;
     } else {
-      console.log('Package version for ' + daemon + ' is the same. No restart required.');
+      console.log(`Package version for ${daemon} is the same.`);
     }
+  }
+  if (shouldRestart) {
+    sh.exec(`sudo service ${daemon} restart`);
+    console.log(`Restarting ${daemon}.`);
+  }
+};
+
+function didFileChange(file) {
+  if (!fs.existsSync(file)) {
+    console.log(`File ${file} does not exist.`);
+  } else {
+    var fileStats = fs.statSync(file);
+    var cacheTime = readCacheModifyTime(file);
+    if (fileStats.mtime.toString() !== cacheTime) {
+      cacheFileModifyTime(file);
+      return true;
+    }
+  }
+  return false;
+}
+
+function cacheFileModifyTime(filepath) {
+  var modStats = fs.statSync(filepath);
+  var cacheName = filepath.replace(/\//g, '-');
+  sh.exec(`mkdir -p ${orchestraCacheHome}`);
+  fs.writeFileSync(`${orchestraCacheHome}/${cacheName}.mtime`, modStats.mtime);
+}
+
+function readCacheModifyTime(filepath) {
+  var cacheName = filepath.replace(/\//g, '-');
+  var cacheFilePath = `${orchestraCacheHome}/${cacheName}.mtime`;
+  if (fs.existsSync(cacheFilePath)) {
+    var cacheContents = fs.readFileSync(cacheFilePath, 'utf8');
+    return cacheContents;
   }
 }
 
 function cacheDaemonVersion(daemon) {
-  sh.exec('apt-cache show ' + daemon + ' > /var/cache/orchestra/' + daemon + '.cache');
+  var daemonCache = sh.exec(`${list} ${daemon}`);
+  console.log(daemonCache.toString());
+  fs.writeFileSync(`${orchestraCacheHome}/${daemon}.cache`, daemonCache);
 }
