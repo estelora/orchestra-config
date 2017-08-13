7a3ef400b5b428ab661882785ae6270c2fdb8a2e
Lydia Joslin <lydia.joslin@mindflash.com>
2017-08-13 14:20:35 -0700

    WIP: preparing for an unhappy server.
    config(bootstrap): added force install for apt packages.
    
    config(bootstrap): moved force install before apt-get update.
    
    orchestra(shelljs): turned off silent mode for debugging.
    
    feat(package): added installation of apache2-bin package.
    
    config: added apt-get upgrade.
    
    chore(bootstrap): corrected update to upgrade.

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,8 +3,14 @@
 # clean the package manager cache
  sudo apt-get clean
 
+# force install packages that may have unmet dependencies
+sudo apt-get -f install
+
 # check for updates
 sudo apt-get -y update
+
+# check for upgrades
+sudo apt-get -y upgrade
 
 # grab source for node 8
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
--- hello-php-arrangement.js
+++ hello-php-arrangement.js
@@ -2,13 +2,24 @@ var orchestra = require('./src/orchestra.js');
 
 var apacheRoot = '/var/www/html';
 var apacheConf = '/etc/apache2/apache2.conf';
-var relevantFiles = [`${apacheRoot}/index.php`, `${apacheRoot}/index.html`, apacheConf];
+var apacheSitesAvailable = '/etc/apache2/conf-available/servername.conf';
+var serverName= 'localhost';
+var hostname = '/etc/hostname';
 
-var php = `<?php 
-  header("Content-Type: text/plain"); 
+var relevantFiles = [
+    `${apacheRoot}/index.php`,
+    `${apacheRoot}/index.html`,
+    apacheConf,
+    apacheSitesAvailable,
+    hostname
+];
+
+var php = `<?php
+  header("Content-Type: text/plain");
   echo "Hello, world!\n"; ?>`;
 
 // Install packages
+orchestra.installPackage('apache2-bin');
 orchestra.installPackage('apache2');
 orchestra.installPackage('libapache2-mod-php5');
 orchestra.installPackage('php5');
@@ -16,6 +27,10 @@ orchestra.installPackage('php5');
 // Configure Apache Files
 orchestra.removeFile(`${apacheRoot}/index.html`, 'apache2');
 orchestra.writeFileContents(`${apacheRoot}/index.php`, php);
+orchestra.appendFileContents(hostname, 'ServerName  localhost');
+
+// Enable Apache2-related Modules
+orchestra.executeCommand('sudo a2enmod auth_basic authn_core authn_file authz_core authz_host authz_user access_compat');
 
 // Restart the Apache service, if needed
 orchestra.restartDaemon('apache2', relevantFiles);
