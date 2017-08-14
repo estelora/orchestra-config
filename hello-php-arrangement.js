2b36c688b928b187607b55147b91d10529e0df43
Lydia Joslin <lydia.joslin@mindflash.com>
2017-08-13 17:51:42 -0700

    cleanup: hello-php-arrangement.js.

--- hello-php-arrangement.js
+++ hello-php-arrangement.js
@@ -3,7 +3,6 @@ var orchestra = require('./src/orchestra.js');
 var apacheRoot = '/var/www/html';
 var apacheConf = '/etc/apache2/apache2.conf';
 var apacheSitesAvailable = '/etc/apache2/conf-available/servername.conf';
-var serverName= 'localhost';
 var hostname = '/etc/hostname';
 
 var relevantFiles = [
@@ -19,7 +18,6 @@ var php = `<?php
   echo "Hello, world!\n"; ?>`;
 
 // Install packages
-orchestra.installPackage('apache2-bin');
 orchestra.installPackage('apache2');
 orchestra.installPackage('libapache2-mod-php5');
 orchestra.installPackage('php5');
@@ -27,10 +25,9 @@ orchestra.installPackage('php5');
 // Configure Apache Files
 orchestra.removeFile(`${apacheRoot}/index.html`, 'apache2');
 orchestra.writeFileContents(`${apacheRoot}/index.php`, php);
-orchestra.appendFileContents(hostname, 'ServerName  localhost');
+orchestra.appendFileContents(apacheConf, 'ServerName    localhost');
+orchestra.writeFileContents(hostname, 'localhost');
 
-// Enable Apache2-related Modules
-orchestra.executeCommand('sudo a2enmod auth_basic authn_core authn_file authz_core authz_host authz_user access_compat');
 
 // Restart the Apache service, if needed
 orchestra.restartDaemon('apache2', relevantFiles);
